3458b9ed556191ae0bf6a01109d4ed6ab41b8495
Andrew-Wyn <terraformer.144@gmail.com>
2018-07-06 18:30:43 +0200

    init

---
+++ Assets/js/js.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    /*ACTIVATOR FOR FRAMEWORK*/
+    //some code!!
+    /********************************************************/
+    
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $(".main").position().top + $(".main").height()){
+            $(".header").addClass("pos_fix");
+            $(".compensatore").removeClass("dn");
+            $(".head_before").addClass("op0");
+            $(".head_after").removeClass("op0");
+        } else {
+            $(".header").removeClass("pos_fix");
+            $(".compensatore").addClass("dn");
+            $(".head_after").addClass("op0");
+            $(".head_before").removeClass("op0");
+        }
+    });
+    
+    
+    /* -------------------------- SCROLL GIUSTO PER I LINK dell'Indice --------------------------- */
+    function rightScroll(anID) {
+        var x = $("#"+anID).offset();
+        $("html,body").animate({scrollTop: x.top + 10}, 1000, function(){});
+    };
+});
